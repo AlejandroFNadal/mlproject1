ac90cfa4090771e641ad81659e9e58963218f72e
AnastaciusWright <32724827+AnastaciusWright@users.noreply.github.com>
2020-02-05 23:19:27 -0300

    Add files via upload

---
+++ gain.py
@@ -0,0 +1,18 @@
+function gain(Set, Attr):
+	ent=calc_entropy(Set)
+	i=0
+	j=0
+	sums=[0,0,0,0]
+	subsets[[],[],[],[]]
+	values=["A","C","T","D"]
+	for j in range(0,len(sums)):
+		
+		for i in range(0,len(Set)):
+			if Set[i][Attr] == values[j]
+				sums[j]++
+				subsets[j].append(Set[i])
+	
+	for j in range(0,len(sums)):
+		right_term=right_term-(sums[j]/len(Set))*calc_entropy(subset[j])
+	
+	return ent-right_term
