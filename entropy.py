073ec887fcbfa510bb83d7903dc3dd6b3ad1aacf
AnastaciusWright <alexandronadal@gmail.com>
2020-02-06 22:21:38 -0300

    Entropy function works

--- entropy.py
+++ entropy.py
@@ -2,13 +2,20 @@ from Set import *
 from math import log2
 def entropy(S):
   L=np.unique(S.clas)
-  c1=len(S.clas==L[0])
-  c2=len(S.clas==L[1])
-  c3=len(S.clas==L[2])
+  print(L)
+  E=0 #contains entropy
+  a=0 #a is index of count
+  count=np.zeros(L.size,dtype=int) #number of elements of each class
+  for x in S.clas:
+    for clas in L:
+      #count[a]=len(S.clas==clas)
+      if x == clas:
+        count[a]+=1
+      a+=1
+    a=0
   cc=S.clas.size
-  l1=log2(c1/cc)*c1/cc
-  l2=log2(c2/cc)*c2/cc
-  l3=log2(c3/cc)*c3/cc
-  E=-(l1+l2+l3)
+  for x in count:
+    E=E+log2(int(x)/cc)*x/cc
+  E=-E
   return E
   
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import numpy as np
+from Set import *
+from entropy import *
+a=np.genfromtxt('mitcheldata.csv',delimiter=',',skip_header=True,dtype=int)
+classColumn=a.shape[1] -1 #Get the last column, that is the class
 
-a=np.genfromtxt('mitcheldata.csv',delimiter=',',skip_header=True)
-print(a)
+#print("Entire dataset")
+#print(a)
+
+data=Set()
+data.clas=a[:,classColumn] #Get the last column
+
+#print("Classes")
+#print(data.clas)
+
+print(entropy(data))
