5950314c4dd93346c486d1099dc7af4fee572b72
AnastaciusWright <alexandronadal@gmail.com>
2020-02-13 15:31:28 -0300

    Generic tree structure

---
+++ tree.py
@@ -0,0 +1,14 @@
+class tree:
+    root=None
+    class_list=[]#list of classes
+
+class node:
+    attr=0 #atribute that is represented by the node
+    value=0 #value of the attribute
+    elements=[] #list of elements that have the value of the attr
+
+    def addChildren(self):
+        x= node()
+        elements.append(x)
+
+    
