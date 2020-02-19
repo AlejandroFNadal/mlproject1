b3715aad2de2eb4c1deb844ac632606640115ce9
AnastaciusWright <32724827+AnastaciusWright@users.noreply.github.com>
2020-02-19 00:28:07 -0300

    eliminated functions that do not work with pandas

--- entropy.py
+++ entropy.py
@@ -20,51 +20,7 @@ def entropy_DF(df): #works with pandas dataframe
     E=E+log2(int(x)/cc)*x/cc
   E=-E
   return E
-  
-def entropy(S):
-  print("Classes")
-  print(S.getClasses())
-  print("---")
-  classes=S.getClasses()
-  L=np.unique(classes)
-  print(L)
-  E=0 #contains entropy
-  a=0 #a is index of count
-  count=np.zeros(L.size,dtype=int) #number of elements of each class
-  for x in classes:
-    for clas in L:
-      #count[a]=len(S.clas==clas)
-      if x == clas:
-        count[a]+=1
-      a+=1
-    a=0
-  cc=classes.size
-  for x in count:
-    E=E+log2(int(x)/cc)*x/cc
-  E=-E
-  return E
-  
-def entropy_list(S):
-  classes=np.array([],dtype=int)
-  for x in S:
-    classes=np.append(classes,[x.clas])
-  L=np.unique(classes)
-  print(L)
-  E=0 #contains entropy
-  a=0 #a is index of count
-  count=np.zeros(L.size,dtype=int) #number of elements of each class
-  for x in classes:
-    for clas in L:
-      #count[a]=len(S.clas==clas)
-      if x == clas:
-        count[a]+=1
-      a+=1
-    a=0
-  cc=classes.size
-  for x in count:
-    E=E+log2(int(x)/cc)*x/cc
-  E=-E
-  return E
+
 def gini_index_list(S):
   classes=np.array([],dtype=int)
   for x in S:
